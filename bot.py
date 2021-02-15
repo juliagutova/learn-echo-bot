63c66bfc59949881cee7fbb2834dc6a7d4fff424
Juliagutova <yulia.program@yandex.ru>
2021-02-15 23:46:52 +0300

    New super echo bot

---
+++ bot.py
@@ -0,0 +1,39 @@
+# Импортируем нужные компаненты
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+# Запись отчета о работе бота
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+# Настройки прокси
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {
+        'username': settings.PROXY_USERNAME,
+        'password': settings.PROXY_PASSWORD
+    }
+}
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+# Функция которая отвечает пользователю
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+# Функция, которая соединяется с плтформой Telegrem, 'тело' бота
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user)) # добавление диспетчеру обработчик, который реагирует на страт и вызывае greet_user
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+main()
